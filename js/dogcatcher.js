64ca16b6e4e71638f62e1803fc23740397b47013
Chris Mazzullo <chris.mazzullo@gmail.com>
2015-02-05 06:42:43 -0500

    Added dogcatchers that follow the player, new dirt texture

--- js/dogcatcher.js
+++ js/dogcatcher.js
@@ -4,7 +4,9 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
+    game.load.image('game_over', 'assets/game_over.png');
     game.load.image('dog', 'assets/dog.png');
+    game.load.image('catcher', 'assets/catcher.png');
     game.load.tilemap('matching', 'assets/phaser_tiles.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', 'assets/phaser_tiles-numbered.png');//, 100, 100, -1, 1, 1);
 }
@@ -42,10 +44,31 @@ var ytiles = 6;
 var start_tile_x = 3;
 var start_tile_y = 5
 var player;
+var score = 0;
+var move_count = 0;
 
-function myalert() {
-    alert('hello');
+function moveCatcher() {
+    if (player.y < catcher.y) {catcher.y -= tile_dim;}
+    else if (player.y > catcher.y) {catcher.y += tile_dim;}
+    else if (player.x < catcher.x) {catcher.x -= tile_dim;}
+    else if (player.x > catcher.x) {catcher.x += tile_dim;}
 }
+
+function moveCatcher2() {
+    if (player.y < catcher2.y) {catcher2.y -= tile_dim;}
+    else if (player.y > catcher2.y) {catcher2.y += tile_dim;}
+    else if (player.x < catcher2.x) {catcher2.x -= tile_dim;}
+    else if (player.x > catcher2.x) {catcher2.x += tile_dim;}
+}
+
+function playerAction() {
+    move_count++;
+    if (move_count % 3 == 0) {
+	moveCatcher();
+	moveCatcher2();
+    }
+}
+
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.input.mouse.enabled = false;
@@ -64,36 +87,39 @@ function create() {
     //player.body.gravity.y = 300
     player.body.collideWorldBounds = true;
     game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
-    this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(function () {processClick();}, this);
-    this.input.keyboard.addKey(Phaser.Keyboard.UP).onDown.add(function () {player.y -= 100;}, this);
-    this.input.keyboard.addKey(Phaser.Keyboard.DOWN).onDown.add(function () {player.y += 100;}, this);
-    this.input.keyboard.addKey(Phaser.Keyboard.RIGHT).onDown.add(function () {player.x += 100;}, this);
-    this.input.keyboard.addKey(Phaser.Keyboard.LEFT).onDown.add(function () {player.x -= 100;}, this);
+    this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(function () {playerAction(); processClick();}, this);
+    this.input.keyboard.addKey(Phaser.Keyboard.UP).onDown.add(function () {playerAction(); player.y -= 100;}, this);
+    this.input.keyboard.addKey(Phaser.Keyboard.DOWN).onDown.add(function () {playerAction(); player.y += 100;}, this);
+    this.input.keyboard.addKey(Phaser.Keyboard.RIGHT).onDown.add(function () {playerAction(); player.x += 100;}, this);
+    this.input.keyboard.addKey(Phaser.Keyboard.LEFT).onDown.add(function () {playerAction(); player.x -= 100;}, this);
+
+    catcher = game.add.sprite(6 * tile_dim, 6 * tile_dim, 'catcher')
+    game.physics.arcade.enable(catcher)
+    catcher.body.collideWorldBounds = true;
+    catcher2 = game.add.sprite(1 * tile_dim, 6 * tile_dim, 'catcher')
+    game.physics.arcade.enable(catcher2)
+    catcher2.body.collideWorldBounds = true;
+}
+
+function game_over() {
+    game.add.sprite(0, 0, 'game_over');
 }
 
 function update() {
 
     countDownTimer();
-
-    // if (layer.getTileX(game.input.activePointer.worldX) <= 5) // to prevent the marker from going out of bounds
-    // {
-    //     marker.x = layer.getTileX(game.input.activePointer.worldX) * 100;
-    //     marker.y = layer.getTileY(game.input.activePointer.worldY) * 100;
-    // }
-
-    if (flipFlag == true)
+    if ((player.x === catcher.x && player.y === catcher.y) || (player.x === catcher2.x && player.y === catcher2.y))
     {
-        if (game.time.totalElapsedSeconds() - timeCheck > 0.5)
-        {
-            flipBack();
-        }
+	game_over();
+    }
+    if (myCountdownSeconds <= 0) {
+	game_over();
     }
 }
 
-
 function countDownTimer() {
 
-    var timeLimit = 120;
+    var timeLimit = 60;
 
     mySeconds = game.time.totalElapsedSeconds();
     myCountdownSeconds = timeLimit - mySeconds;
@@ -118,7 +144,10 @@ function processClick() {
         flipOver();
         squareCounter++;
     }
-
+    if (getHiddenTile() < 9)
+    {
+	score++;
+    }
 }
 
 function flipOver() {
@@ -185,10 +214,10 @@ function render() {
     game.debug.text(timesUp, 620, 208, 'rgb(0,255,0)');
     game.debug.text(youWin, 620, 240, 'rgb(0,255,0)');
 
-    game.debug.text('Time: ' + myCountdownSeconds, 620, 15, 'rgb(0,255,0)');
+    game.debug.text('Time: ' + Math.round(myCountdownSeconds), 620, 15, 'rgb(0,255,0)');
 
     //game.debug.text('squareCounter: ' + squareCounter, 620, 272, 'rgb(0,0,255)');
-    game.debug.text('Matched Pairs: ' + masterCounter, 620, 304, 'rgb(0,0,255)');
+    //game.debug.text('Matched Pairs: ' + masterCounter, 620, 304, 'rgb(0,0,255)');
 
     //game.debug.text('startList: ' + myString1, 620, 208, 'rgb(255,0,0)');
     //game.debug.text('squareList: ' + myString2, 620, 240, 'rgb(255,0,0)');
@@ -199,6 +228,6 @@ function render() {
     game.debug.text('LayerX: ' + layer.getTileX(player.x), 620, 80, 'rgb(255,0,0)');
     game.debug.text('LayerY: ' + layer.getTileY(player.y), 620, 112, 'rgb(255,0,0)');
 
-    game.debug.text('Tile Position: ' + currentTilePosition, 620, 144, 'rgb(255,0,0)');
-    game.debug.text('Hidden Tile: ' + getHiddenTile(), 620, 176, 'rgb(255,0,0)');
+    game.debug.text('Move Count: ' + move_count, 620, 144, 'rgb(255,0,0)');
+    game.debug.text('Score: ' + score, 620, 176, 'rgb(255,0,0)');
 }
