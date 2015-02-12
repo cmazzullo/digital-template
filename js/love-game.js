d7341ed405fb563af7d3c7eab56f69b4fe7ca022
Chris Mazzullo <chris.mazzullo@gmail.com>
2015-02-11 20:53:21 -0500

    Removed unnecessary load

--- js/love-game.js
+++ js/love-game.js
@@ -38,8 +38,6 @@ boot.prototype = {
 
 	this.game.load.image('ground', 'assets/platform.png');
 	this.game.load.image("main", "assets/background3.png");
-	this.game.load.tilemap('level1', 'assets/level1.json', null,
-			       Phaser.Tilemap.TILED_JSON);
 	this.game.load.image('tiles-1', 'assets/tiles-1.png');
 	this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 	this.game.load.spritesheet('butter', 'assets/butter.png', 26, 48);
